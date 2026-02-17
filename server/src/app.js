831dce17f8d72567a2722ce53b3d086e15ebc2a0
hemantsaini404 <>
2026-02-17 14:47:58 +0530

    Auth backend updates

---
+++ server/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const resourcesRouter = require('./routes/resources');
+const usersRouter = require('./routes/users');
+
+app.use('/resources', resourcesRouter);
+app.use('/users', usersRouter);
+
+
+// Routes Placeholder
+app.get('/', (req, res) => {
+
+    res.send('Butter Barbies Backend is doing great!');
+});
+
+module.exports = app;
